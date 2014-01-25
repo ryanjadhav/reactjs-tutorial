e21ac0c79cb2bbd1a69d4b3b43843b45bab2d1c9
ryanjadhav <ryan.jadhav@gmail.com>
2014-01-25 13:44:07 -0800

    removed uneccessary json

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ app.get('/comments.json', function(req, res) {
 });
 
 app.post('/comments.json', function(req, res) {
+  console.log(comments);
   comments.push(req.body);
+  console.log(req.body);
+  console.log(comments);
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(comments));
 });
